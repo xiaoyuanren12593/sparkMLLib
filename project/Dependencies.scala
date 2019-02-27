cfbfba8512394dfdceead7b5b1c35fc3bbb1cc96
suxin <suxin@baozhunniu.com>
2019-02-27 11:21:05 +0800

    解决每次发布需要手动修改sbt的问题

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,29 +10,9 @@ object Dependencies {
   val fastjson = "com.alibaba" % "fastjson" % "1.2.24"
   // joda-time
   val jodaTime = "joda-time" % "joda-time" % "2.3"
-
   // spark 中文分词
   val ansjSeg = "org.ansj" % "ansj_seg" % "5.1.1"
   val nlpLang = "org.nlpcn" % "nlp-lang" % "1.7.2"
-
-  //  hive
-  val sparkHive = "org.apache.spark" %% "spark-hive" % "1.6.1"
-  //  spark-neo4j
-  val sparkNeo4j = "neo4j-contrib" % "neo4j-spark-connector" % "2.1.0-M4"
-  //  spark-csv
-  val sparkCsv = "com.databricks" %% "spark-csv" % "1.4.0"
-  // sparkStreaming
-  val sparkStreaming = "org.apache.spark" %% "spark-streaming" % "1.6.1"
-  val sparkStreamingKafka = "org.apache.spark" %% "spark-streaming-kafka" % "1.6.1"
-  // spark-mllib
-  val sparkMllib = "org.apache.spark" % "spark-mllib_2.10" % "1.6.1"
-  //  hbase
-  val hbaseClient = "org.apache.hbase" % "hbase-client" % "1.2.0"
-  val hbaseCommon = "org.apache.hbase" % "hbase-common" % "1.2.0"
-  val hbaseServer = "org.apache.hbase" % "hbase-server" % "1.2.0"
-  val hbaseHadoopCompat = "org.apache.hbase" % "hbase-hadoop-compat" % "1.2.0"
-
-  //---------------------------------------------以下打包使用------------------------------------------------------------
   //  hive
   val sparkHiveProvided = "org.apache.spark" %% "spark-hive" % "1.6.1" % "provided"
   //  spark-neo4j
@@ -49,17 +29,8 @@ object Dependencies {
   val hbaseCommonProvided = "org.apache.hbase" % "hbase-common" % "1.2.0" % "provided"
   val hbaseServerProvided = "org.apache.hbase" % "hbase-server" % "1.2.0" % "provided"
   val hbaseHadoopCompatProvided = "org.apache.hbase" % "hbase-hadoop-compat" % "1.2.0" % "provided"
-  //---------------------------------------------以上打包使用------------------------------------------------------------
 
   //--------------------------------------------全依赖-----------------------------------------------------------------
-  val allDeps = Seq(neo4jJavaDriver, mysqlConnectorJava, jedis, fastjson, ansjSeg, nlpLang,
-    sparkHive, sparkNeo4j, sparkCsv, sparkStreaming, sparkStreamingKafka, sparkMllib, hbaseClient,
-    hbaseCommon, hbaseServer, hbaseHadoopCompat).map(
-    _.excludeAll(ExclusionRule(organization = "org.mortbay.jetty"))
-  ).map(
-    _.excludeAll(ExclusionRule(organization = "javax.servlet"))
-  )
-
   val allDepsProvided = Seq(neo4jJavaDriver, mysqlConnectorJava, jedis, fastjson, ansjSeg, nlpLang,
     sparkHiveProvided, sparkNeo4jProvided, sparkCsvProvided, sparkStreamingProvided,
     sparkStreamingKafkaProvided, sparkMllibProvided, hbaseClientProvided, hbaseCommonProvided, hbaseServerProvided,
@@ -69,44 +40,31 @@ object Dependencies {
   val utilDeps = Seq(jodaTime)
 
   //---------------------------------------------企业模块标签------------------------------------------------------------
-  // 企业标签打包时需要的引用
-  val enterpriseDeps = Seq(mysqlConnectorJava, fastjson, jodaTime, sparkHive, hbaseClient, hbaseServer, hbaseCommon)
   // 企业标签本地引用
   val enterpriseDepsProvided = Seq(mysqlConnectorJava, fastjson, jodaTime, sparkHiveProvided, hbaseClientProvided,
     hbaseServerProvided, hbaseCommonProvided)
 
-  //---------------------------------------------个人模块标签------------------------------------------------------------
-  // 个人标签本地引用
-  val entvaluePersonrisklDeps = Seq(fastjson, sparkHive, sparkMllib, hbaseClient, hbaseServer, hbaseCommon)
+  //---------------------------------------------企业价值个人风险标签------------------------------------------------------------
   // 个人标签打包时需要的引用
-  val entvaluePersonrisklDepsProvided = Seq(fastjson, sparkHive, sparkMllibProvided, hbaseClientProvided,
+  val entvaluePersonrisklDepsProvided = Seq(fastjson, sparkHiveProvided, sparkMllibProvided, hbaseClientProvided,
     hbaseServerProvided, hbaseCommonProvided)
 
   //---------------------------------------------etl bi3模块标签-------------------------------------------------------
-  // 个人标签打包时需要的引用
-  val etlBi3Deps = Seq(mysqlConnectorJava, fastjson, sparkHive, sparkMllib, hbaseClient, hbaseServer, hbaseCommon)
   // 个人标签本地引用
   val etlBi3DepsProvided = Seq(mysqlConnectorJava, fastjson, sparkHiveProvided, sparkMllibProvided,
     hbaseClientProvided, hbaseServerProvided, hbaseCommonProvided)
 
   //---------------------------------------------etl piwik模块标签-------------------------------------------------------
-  // 个人标签打包时需要的引用
-  val etlPiwikDeps = Seq(mysqlConnectorJava, sparkHive, sparkMllib, hbaseClient, hbaseServer, hbaseCommon)
   // 个人标签本地引用
   val etlPiwikDepsProvided = Seq(mysqlConnectorJava, sparkHiveProvided, sparkMllibProvided, hbaseClientProvided,
     hbaseServerProvided, hbaseCommonProvided)
 
   //---------------------------------------------etl redis模块标签-------------------------------------------------------
-  // 个人标签打包时需要的引用
-  val etlRedisDeps = Seq(neo4jJavaDriver, mysqlConnectorJava, jedis, fastjson, ansjSeg, sparkStreaming, 
-    sparkStreamingKafka, sparkMllib, sparkHive, sparkMllib)
   // 个人标签本地引用
   val etlRedisDepsProvided = Seq(mysqlConnectorJava, sparkHiveProvided, sparkMllibProvided, hbaseClientProvided,
     hbaseServerProvided, hbaseCommonProvided)
 
   //---------------------------------------------个人模块标签------------------------------------------------------------
-  // 企业标签打包时需要的引用
-  val personalDeps = Seq(fastjson, sparkHive, hbaseClient, hbaseServer, hbaseCommon)
   // 企业标签本地引用
   val personalDepsProvided = Seq(fastjson, sparkHiveProvided, hbaseClientProvided, hbaseServerProvided, hbaseCommonProvided)
 }
